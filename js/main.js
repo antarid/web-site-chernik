ae075064c502eb511d7ba45a6d1d33e4efc4dad9
antarid <tarand007@gmail.com>
2017-12-21 22:15:49 +0300

    smth changed

--- js/main.js
+++ js/main.js
@@ -8,6 +8,19 @@ $(document).ready(function(){
     loop: true,
     startDelay: 1
   });
+
+  $("#scenary").typed({
+    strings: ["We will make you a scenary", "It will be something like this" ,"...", "...",
+     "First verse of scenary" ,"..." ,"..." ,"Oh no, it's bad",
+     "Second verse of scenary" ,"..." ,"..." ,"Oh, it's better",
+     "Fine" , "We are sending it to you" , "Поставьте зачет", "))))"],
+    typeSpeed: 12,
+    backDelay: 900,
+    backSpeed: 1,
+    loop: true,
+    startDelay: 1
+  });
+  
   
   let header = document.getElementsByTagName('header')[0];
   window.onscroll = () => {
